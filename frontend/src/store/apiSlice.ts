c9c80ad2dc091ff55e3bbe37829e697250ab39fb
mrzahussnain <hussnain610a@gmail.com>
2026-01-26 21:15:18 +0000

    chore: Fixed Base backend Api calling

--- frontend/src/store/apiSlice.ts
+++ frontend/src/store/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"; 
 
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const API_URL = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`;
 
 export const apiSlice = createApi({
   reducerPath: "api",
