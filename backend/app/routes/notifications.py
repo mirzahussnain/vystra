d26b4af633e27d09b4bd4c48294b1b70e1f78278
mrzahussnain <hussnain610a@gmail.com>
2026-01-27 00:04:19 +0000

    chore: Fixed notifications backend routes, added user validation check to perform crud to current logged in user

--- backend/app/routes/notifications.py
+++ backend/app/routes/notifications.py
@@ -7,15 +7,15 @@ from ..database.models import Notification
 router=APIRouter()
 
 @router.get("/notifications")
-def get_notifications(db:Session=Depends(get_db),limit:int=5):
+def get_notifications(db:Session=Depends(get_db),user:User=Depends(get_current_user),limit:int=5):
     """Get the 5 most recent notifications"""
-    notifications=db.query(Notification).order_by(Notification.created_at.desc()).limit(limit).all()
+    notifications=db.query(Notification).filter(Notification.user_id==user.id).order_by(Notification.created_at.desc()).limit(limit).all()
     return notifications
     
 @router.patch("/notifications/{id}/read")
-def mark_read(id: int, db: Session = Depends(get_db)):
+def mark_read(id: int,user:User=Depends(get_current_user), db: Session = Depends(get_db)):
     """Mark a notification as read (turns off the red dot)"""
-    notif = db.get(Notification,id)
+    notif = db.query(Notification).filter(Notification.id==id,Notification.user_id==user.id).first()
     
     if not notif:
         
@@ -29,11 +29,14 @@ def mark_read(id: int, db: Session = Depends(get_db)):
     return {"status": True}
 
 @router.put("/notifications/mark-all-read")
-def mark_all_read(db: Session = Depends(get_db)):
+def mark_all_read(user:User=Depends(get_current_user),db: Session = Depends(get_db)):
     """Mark ALL unread notifications as read (for the 'Mark all' button)"""
-    db.query(Notification)\
-      .filter(Notification.is_read == False)\
-      .update({"is_read": True})
-      
+    notif=db.query(Notification).filter(Notification.user_id==user.id,not Notification.is_read == False)
+    if not notif:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="No unread notifications"
+        )
+    notif.update({"is_read": True})
     db.commit()
     return {"status": True, "message": "All marked as read"}
